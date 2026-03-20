6ac16189e1f36faea919ec5110c11776c94d93f9
eritho91 <erik.thorell@iths.se>
2026-03-20 13:36:56 +0100

    Lagt till maven.yml

---
+++ src/main/java/se/iths/erikthorell/laboration_enhetstestning/LaborationEnhetstestningApplication.java
@@ -0,0 +1,13 @@
+package se.iths.erikthorell.laboration_enhetstestning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaborationEnhetstestningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaborationEnhetstestningApplication.class, args);
+    }
+
+}
